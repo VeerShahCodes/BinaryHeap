79bf219c73c9cc3c09c481250967f7dd12ddc7a4
VeerShahCodes <shahveer1213@gmail.com>
2024-11-17 12:01:48 -0800

    made helper functions for the heap tree and started the basics

---
+++ BinaryHeap/HeapTree.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryHeap
+{
+    public class HeapTree<T>
+        where T : IComparable<T>
+    {
+
+        T[] arr = new T[10];
+        public int Count { get; private set; }
+        
+        public HeapTree()
+        {
+
+        }
+
+        public void Insert(T val)
+        {
+            arr[Count] = val;
+            if(Count >= arr.Length)
+            {
+                T[] newArr = new T[arr.Length * 2];
+                arr = newArr;
+                arr[Count] = val;
+            }
+            HeapifyUp(arr, Count);
+            Count++;
+        }
+
+        public void HeapifyUp(T[] arr, int Count)
+        {
+
+        }
+
+        public void Pop()
+        {
+
+        }
+
+        public void HeapifyDown()
+        {
+
+        }
+
+        public int GetParent(int index)
+        {
+            return index / 2;
+        }
+
+
+        private int GetLeftChild(int index)
+        {
+            return index * 2 + 1;
+        }
+
+        private int GetRightChild(int index)
+        {
+            return index * 2 + 2;
+        }
+    }
+}
--- BinaryHeap/Program.cs
+++ BinaryHeap/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Worldeeeee!");
+            HeapTree<int> heap = new HeapTree<int>();
+            heap.Insert(1);
+            heap.Insert(5);
+            Console.WriteLine(heap.GetParent(1));
+
+
+
+
         }
     }
 }
