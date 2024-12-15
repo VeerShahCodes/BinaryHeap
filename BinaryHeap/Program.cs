8a632af25a58afcec1ee9a32580a4e14f513d667
Veer.Shah <shahveer1213@gmail.com>
2024-12-15 14:30:33 -0800

    it works

--- BinaryHeap/Program.cs
+++ BinaryHeap/Program.cs
@@ -9,7 +9,7 @@ namespace BinaryHeap
             Console.WriteLine("Hello, Worldeeeee!");
             HeapTree<int> heap = new HeapTree<int>();
 
-            List<int> list = new List<int>([5, 687, 34, 7, 4, 7, 4, 5, 3, 5, 7,]);
+            List<int> list = new List<int>([5, 3, 7, 8, 1, 9, 10, 12, 11, 13, 6, 100, 76, 54]);
             list = HeapSort(list, heap);
             for(int i = 0; i < list.Count; i++)
             {
